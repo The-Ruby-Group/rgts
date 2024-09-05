8683e575d074347be317973ccf4b4fac9706a960
Jackson Jessup <jessupjn@umich.edu>
2024-09-05 15:02:00 -0400

    Add example project

---
+++ example-vite-app/eslint.config.mjs
@@ -0,0 +1,9 @@
+// Import the configuration from @the-ruby-group/rgts
+import rubyGroupConfig from '@the-ruby-group/rgts/eslint.config.js';
+
+export default [
+  ...rubyGroupConfig,
+  /**
+   * NOTE: Extra project-specific ESLint configurations can be added here...
+   */
+];
---
+++ example-vite-app/prettier.config.mjs
@@ -0,0 +1,9 @@
+import rubyGroupConfig from '@the-ruby-group/rgts/prettier.config.js';
+
+/** @type {import("prettier").Config} */
+export default {
+  ...rubyGroupConfig,
+  /**
+   * NOTE: Extra project-specific settings can be added here...
+   */
+};
